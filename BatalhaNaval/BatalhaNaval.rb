3809825948ba12554c124d27791eaa38b29b6481
Guarajuba <gcwarken@gmail.com>
2017-12-01 16:57:24 -0200

    Main.rb is now BatalhaNaval.rb

---
+++ BatalhaNaval/BatalhaNaval.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+$LOAD_PATH << '.'
+# require all files in current folder
+Dir["./*.rb"].each { |file| require file unless file == "./BatalhaNaval.rb"}
+
+module NavalBattle
+  mine_size = 1
+  sub_size  = 2
+  ship_size = 3
+
+  print "Select board size: "
+  board_size = gets.chomp.to_i
+
+  ships = Array.new()
+
+  print "How many mines of size #{mine_size}?\t"
+  mine_quant = gets.chomp.to_i
+  for i in 0..mine_quant - 1
+    ships.push(mine_size)
+  end
+
+
+  print "How many submarines of size #{sub_size}?\t"
+  submarine_quant = gets.chomp.to_i
+  for i in 0..submarine_quant - 1
+    ships.push(sub_size)
+  end
+
+  print "How many ships of size #{ship_size}?\t"
+  ship_quant = gets.chomp.to_i
+  for i in 0..mine_quant - 1
+    ships.push(ship_size)
+  end
+
+  game = GameControl.new()
+  game.new_game(board_size)
+
+  for curr_player in 1..2
+    ships.each do |ship_size|
+      begin
+        ship_position = Array.new([rand(board_size), rand(board_size)])
+        horizontal_oriented = [true, false].sample
+        curr_ship = Ship.new(ship_size, ship_position, horizontal_oriented)
+      end while game.add_ship(curr_ship, curr_player)
+
+      puts "ship of size #{ship_size} added to player #{curr_player}!"
+    end
+  end
+
+
+end
--- BatalhaNaval/Board.rb
+++ BatalhaNaval/Board.rb
@@ -1,13 +1,13 @@
 module NavalBattle
   class Board
-    @board_size
+    @size
     @board_matrix
     attr_reader :size
 
     # constructor
     def initialize(size)
-      @board_size = size
-      @board_matrix = Array.new(@board_size, Array.new(@board_size, false))
+      @size = size
+      @board_matrix = Array.new(@size, Array.new(@size, false))
     end
 
     def is_occupied(position_x, position_y)
